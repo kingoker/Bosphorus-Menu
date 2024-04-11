fcb93c560921c9374c0b2ded5be13b0e1c91a6a8
Akbar <zolotojznak@gmail.com>
2024-04-11 22:53:20 +0200

    version 2.0

--- js/script.js
+++ js/script.js
@@ -1,62 +1,80 @@
-// Меню бургер
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.header__body');
-const menuItem = document.querySelectorAll('.sidebar__link');
+// Dish popup
+document.querySelectorAll('.menu__item').forEach(function(item) {
+    const menuItemContent = item.querySelector('.menu__item_content');
+    const popup = item.querySelector('.popup');
 
-if(iconMenu){
-    iconMenu.addEventListener("click", function(e){
-        document.body.classList.toggle('lock');
-        iconMenu.classList.toggle('active');
-        menuBody.classList.toggle('active');
+    menuItemContent.addEventListener('click', function(event) {
+        const computedStyle = window.getComputedStyle(popup);
+        if (computedStyle.display === 'none') {
+            closeAllPopups();
+            popup.style.display = 'block';
+        } else {
+            popup.style.display = 'none';
+        }
     });
-    menuItem.forEach(item => {
-        item.addEventListener('click', event => {
-            document.body.classList.remove('lock');
-            iconMenu.classList.remove('active');
-            menuBody.classList.remove('active');
-        })
-    })
-}
 
-
-// Изменение аквтивного пункта меню
-var menu = document.getElementById('header__list');
-var links = menu.getElementsByClassName('sidebar__link');
-
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', setActiveLink);
-}
-
-function setActiveLink(event) {
-    for (var i = 0; i < links.length; i++) {
-        links[i].classList.remove('sidebar__link-active');
-    }
-
-    event.target.classList.add('sidebar__link-active');
-}
-
-
-
-
-// Плавный скрол к якорным ссылкам
-var links = document.querySelectorAll('a[href^="#"]');
-
-function handleLinkClick(event) {
-    event.preventDefault();
-
-    var targetId = this.getAttribute('href').slice(1);
-    var targetElement = document.getElementById(targetId);
-    
-    if (targetElement) {
-        var offset = targetElement.getBoundingClientRect().top - 100;
-        
-        window.scrollTo({
-            top: offset,
-            behavior: 'smooth'
+    function closeAllPopups() {
+        document.querySelectorAll('.popup').forEach(function(popup) {
+            popup.style.display = 'none';
         });
     }
-}
+});
 
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', handleLinkClick);
+// Close Popup
+document.querySelectorAll('.popup__close').forEach(function(closeButton) {
+    closeButton.addEventListener('click', function() {
+        const popup = this.closest('.popup');
+        popup.style.display = 'none';
+        document.body.style.overflow = 'auto'; // Возвращаем скроллинг фона
+    });
+});
+
+// Don't scroll
+document.querySelectorAll('.menu__item_content').forEach(function(menuItemContent) {
+    menuItemContent.addEventListener('click', function(event) {
+        const popup = this.nextElementSibling; // Получаем следующий элемент после .menu__item_content, который является .popup
+        popup.style.display = 'block';
+        document.body.style.overflow = 'hidden'; // Отключаем скроллинг фона
+        event.stopPropagation(); // Остановка всплытия события, чтобы не срабатывал обработчик события на документе
+    });
+});
+
+// Close Popup when clicking outside
+document.addEventListener('click', function(event) {
+    const popups = document.querySelectorAll('.popup');
+    const popupContents = document.querySelectorAll('.popup__content');
+    let clickedInsidePopupContent = false;
+
+    popupContents.forEach(function(popupContent) {
+        if (popupContent.contains(event.target)) {
+            clickedInsidePopupContent = true;
+        }
+    });
+
+    if (!clickedInsidePopupContent) {
+        popups.forEach(function(popup) {
+            popup.style.display = 'none';
+            document.body.style.overflow = 'auto'; // Возвращаем скроллинг фона
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+// Dish popup photos
+var thumbs = document.querySelectorAll('.popup__photos_thumbs > a');
+var big = document.querySelector('.popup__photos_main img');
+
+for (var i = 0; i < thumbs.length; i++) {
+    thumbs[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        big.src = this.href;
+    });
 }
